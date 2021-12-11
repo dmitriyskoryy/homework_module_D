fd05dce5bfbfc162e5af72685feb0aad0c352557
Dmitrii Skoryi <progdebug39@gmail.com>
2021-12-11 17:19:31 +0200

    In models.py app news, add in model Author - method update_rating, add in model Post - methods like, dislike, preview

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -3,10 +3,30 @@ from django.db import models
 #User импортируется из django.contrib.auth.models
 from django.contrib.auth.models import User
 
+from django.db.models import Sum
+
+
 class Author(models.Model):
     #Взять один ко дному Автора и Юзера
     authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
     ratingAuthor = models.SmallIntegerField(default=0)
+
+    # с помощью post_set.all() получаем все связанные посты
+    # метод aggregate служит для работы с множеством записей
+    # создаем значение postRating, куда присваиваем сумму всех значений поля rating
+    def update_rating(self):
+        postRat = self.post_set.all().aggregate(postRating=Sum('rating'))
+        pRat = 0
+        #получаем сумму поля rating связанного с автором
+        pRat += postRat.get('postRating')
+
+        commentRat = self.authorUser.comment_set.all().aggrigagte(commentRating=Sum('rating'))
+        cRat = 0
+        cRat += commentRat.get('commentRating')
+
+        self.ratingAuthor = pRat * 3 + cRat
+        self.save()
+
 
 
 class Category(models.Model):
@@ -16,6 +36,7 @@ class Category(models.Model):
 class Post(models.Model):
     #связь один ко многим с моделью Author
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
     NEWS = 'NW'
     ARTICLE = 'AR'
     CATEGORY_CHOICES = (
@@ -23,9 +44,12 @@ class Post(models.Model):
         (ARTICLE, 'Статья'),
     )
     categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+
     dateCreation = models.DateTimeField(auto_now_add=True)
+
     # связь один ко многим с моделью Category, through - промежуточная модель PostCategory
     postCategory = models.ManyToManyField(Category, through='PostCategory')
+
     title = models.CharField(max_length=128)
     text = models.TextField()
     rating = models.SmallIntegerField(default=0)
@@ -52,6 +76,7 @@ class PostCategory(models.Model):
 
 class Comment(models.Model):
     commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+
     # комментарии могут оставлять все, поэтому связь с User, а не с Author
     commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
