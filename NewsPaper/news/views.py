dbdc42beb9f9087852c9d3a3b76e4e122b1661fc
Dmitrii Skoryi <progdebug39@gmail.com>
2021-12-19 15:00:44 +0200

    completion of the D3 module and add file __init__.py, custom_filters.py in directory: news/templatetags

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,7 @@
 #  views.generic позволит выводить все объекты из БД в браузер "в HTML"
 from django.views.generic import ListView, DetailView
 from .models import *
+from datetime import datetime
 
 # пишем представление
 class PostList(ListView):
@@ -8,14 +9,22 @@ class PostList(ListView):
     model = Post
     # указываем имя шаблона, в котором будет лежать html, в котором будут все инструкции о том,
     # как именно пользователю должны вывеститсь наши объекты
-    template_name = 'newsall.html'
+    template_name = 'news.html'
     # это имя списка, в котором будут лежать все объекты, его надо указать,
     # чтобы обратиться к самоу списку объектов через html-шаблон
-    context_object_name = 'newsall'
+    context_object_name = 'news'
+    ordering = ['-id']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now()
+        # context['value1'] = None
+        return context
 
 
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'news.html'
-    context_object_name = 'news'
+    template_name = 'newsone.html'
+    context_object_name = 'newsone'
+
